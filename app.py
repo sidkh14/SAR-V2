a1852f8804134463240e144482a9253af4641543
193006 <137424083+193006@users.noreply.github.com>
2023-08-14 10:04:47 +0000

    case details in doc

--- app.py
+++ app.py
@@ -663,11 +663,11 @@ with st.spinner("Downloading...."):
     # Addition of case details
     case_info = {
         "Case Number                   ": "SAR-2023-24680",
-        "Customer Name                 ": "John Brown",
-        "Customer ID                   ": "9659754",
+        "Customer Name              ": "John Brown",
+        "Customer ID                     ": "9659754",
         "Case open date                ": "Jun 10, 2023",
-        "Case Type                     ": "Fraud Transaction",
-        "Case Status                   ": "Open"
+        "Case Type                         ": "Fraud Transaction",
+        "Case Status                       ": "Open"
     }
     for key_c, value_c in case_info.items():
         doc.add_paragraph(f"{key_c}:{value_c}")
