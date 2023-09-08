bf089a72ecd1ce429e405f658bceaec0c7830e8e
corprt <143188080+corprt@users.noreply.github.com>
2023-09-08 09:48:10 +0000

    summ changes in llama

--- app.py
+++ app.py
@@ -1131,16 +1131,15 @@ if st.session_state.llm == "GPT-3.5":
             st.write(st.session_state["tmp_summary"])
 
 elif st.session_state.llm == "Llama-2":
-    with st.spinner('Summarization ...'):
-        if st.button("Summarize",disabled=st.session_state.disabled):
-            summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
-            query = "Provide a detailed summary of the text provided"
-            prompt_1 = f'''You are responding to a highly professional customer. Give a detailed summary.\n\n\
-                Question: {query}\n\
-                Context: {summ_dict}\n\                      
-                Response: (Provide a detailed summary in a single paragraph.Keep the fontnsize same for every sentence in the summary.Maintain proper spacing\gap between words in the paragraph.)'''
-            summ = llama_llm(llama_13b,prompt_1)  
-            st.write(summ)
+    if st.button("Summarize",disabled=st.session_state.disabled):
+        summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
+        query = "Provide a detailed summary of the text provided"
+        prompt_1 = f'''You are responding to a highly professional customer. Give a detailed summary.\n\n\
+            Question: {query}\n\
+            Context: {summ_dict}\n\                      
+            Response: (Provide a detailed summary in a single paragraph.Keep the font size same for every sentence in the summary.Maintain proper spacing\gap between words in the paragraph.)'''
+        summ = llama_llm(llama_13b,prompt_1)  
+        st.write(summ)
 
 with st.spinner("Downloading...."):
 # if st.button("Download Response", disabled=st.session_state.disabled):
