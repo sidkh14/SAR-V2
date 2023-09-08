f19ebcbc00598ec5fe418895766f4f1fcfd3a337
corprt <143188080+corprt@users.noreply.github.com>
2023-09-08 09:51:32 +0000

    summ changes in llama

--- app.py
+++ app.py
@@ -1134,7 +1134,7 @@ elif st.session_state.llm == "Llama-2":
     if st.button("Summarize",disabled=st.session_state.disabled):
         summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
         query = "Provide a detailed summary of the text provided"
-        prompt_1 = f'''You are responding to a highly professional customer. Give a detailed summary.\n\n\
+        prompt_1 = f'''You are responding to a highly professional customer. Give a detailed summary from the given context.\n\n\
             Question: {query}\n\
             Context: {summ_dict}\n\                      
             Response: (Provide a detailed summary in a single paragraph.)'''
