82a019b813c294022877f631ce82516b86b4ddde
corprt <143188080+corprt@users.noreply.github.com>
2023-09-08 09:39:36 +0000

    prompt changes in llama

--- app.py
+++ app.py
@@ -828,7 +828,7 @@ with st.spinner('Wait for it...'):
                 Do not provide any extra [Explanation, Note] block below the Response.\n\n\
                             Question: {query}\n\
                             Context: {context_1}\n\
-                            Response: (Provide a concise Response in one sentence. Do not provide any extra Note after the response.)'''
+                            Response: (Provide a concise Response in one sentence. Do not provide any extra 'Note','Description".)'''
                 response = llama_llm(llama_13b,prompt_1)
                 chat_history[query] = response
 
