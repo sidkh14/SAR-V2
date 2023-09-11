74424b49feb1cbcec6cbe47dbbcc18269b708fd5
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 14:14:22 +0000

    report changes

--- app.py
+++ app.py
@@ -1140,51 +1140,51 @@ elif st.session_state.llm == "Llama-2-13b":
             st.session_state.tmp_table_llama.drop_duplicates(subset=['Question'])
 
 
-if st.session_state.llm == "GPT-3.5":
+
     with st.spinner('Summarization ...'):  
         if st.button("Summarize",disabled=st.session_state.disabled):
-            summ_dict_gpt = st.session_state.tmp_table_gpt.set_index('Question')['Answer'].to_dict()
-            # chat_history = resp_dict_obj['Summary']
-            memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=300)
-            memory.save_context({"input": "This is the entire summary"}, 
-                            {"output": f"{summ_dict_gpt}"})
-            conversation = ConversationChain(
-            llm=llm, 
-            memory = memory,
-            verbose=True)
-            st.session_state["tmp_summary_gpt"] = conversation.predict(input="Give me a detailed summary of the above texts in a single paragraph without anything additional other than the overall content. Please don't include words like these: 'chat summary', 'includes information' in my final summary.")
-            # showing the text in a textbox
-            # usr_review = st.text_area("", value=st.session_state["tmp_summary_gpt"])
-            # if st.button("Update Summary"):
-            #     st.session_state["fin_opt"] = usr_review
-            st.write(st.session_state["tmp_summary_gpt"])
+            if st.session_state.llm == "GPT-3.5":
+                summ_dict_gpt = st.session_state.tmp_table_gpt.set_index('Question')['Answer'].to_dict()
+                # chat_history = resp_dict_obj['Summary']
+                memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=300)
+                memory.save_context({"input": "This is the entire summary"}, 
+                                {"output": f"{summ_dict_gpt}"})
+                conversation = ConversationChain(
+                llm=llm, 
+                memory = memory,
+                verbose=True)
+                st.session_state["tmp_summary_gpt"] = conversation.predict(input="Give me a detailed summary of the above texts in a single paragraph without anything additional other than the overall content. Please don't include words like these: 'chat summary', 'includes information' in my final summary.")
+                # showing the text in a textbox
+                # usr_review = st.text_area("", value=st.session_state["tmp_summary_gpt"])
+                # if st.button("Update Summary"):
+                #     st.session_state["fin_opt"] = usr_review
+                st.write(st.session_state["tmp_summary_gpt"])
 
-elif st.session_state.llm == "Llama-2-13b":
-    if st.button("Summarize",disabled=st.session_state.disabled):
-        # summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
-        # query = "Provide a detailed summary of the text provided"
-        # prompt_1 = f'''You are a fraud analyst. Analyse the text provided to give a detailed summary by reframing the sentences to create a sequence of events.\n\n\
-        #     Question: {query}\n\
-        #     Context: {summ_dict}\n\                      
-        #     Response: (Provide the summary in a single paragraph with proper spacing between words.Keep font size same for each word )'''
-        # summ = llama_llm(llama_13b,prompt_1)  
-        # st.write(summ)
+            elif st.session_state.llm == "Llama-2-13b":
+                # summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
+                # query = "Provide a detailed summary of the text provided"
+                # prompt_1 = f'''You are a fraud analyst. Analyse the text provided to give a detailed summary by reframing the sentences to create a sequence of events.\n\n\
+                #     Question: {query}\n\
+                #     Context: {summ_dict}\n\                      
+                #     Response: (Provide the summary in a single paragraph with proper spacing between words.Keep font size same for each word )'''
+                # summ = llama_llm(llama_13b,prompt_1)  
+                # st.write(summ)
 
 
-        template = """Write a detailed summary. 
-        Return your response in a single paragraph.
-        ```{text}```
-        Response: """
-        prompt = PromptTemplate(template=template,input_variables=["text"])
-        llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
+                template = """Write a detailed summary. 
+                Return your response in a single paragraph.
+                ```{text}```
+                Response: """
+                prompt = PromptTemplate(template=template,input_variables=["text"])
+                llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
 
-        summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
-        text = []
-        for key,value in summ_dict_llama.items():
-            text.append(value)
-        st.session_state["tmp_summary_llama"] = llm_chain_llama.run(text)
-        st.write(st.session_state["tmp_summary_llama"])
-    
+                summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
+                text = []
+                for key,value in summ_dict_llama.items():
+                    text.append(value)
+                st.session_state["tmp_summary_llama"] = llm_chain_llama.run(text)
+                st.write(st.session_state["tmp_summary_llama"])
+            
 
 
 
@@ -1458,7 +1458,7 @@ with st.spinner("Downloading...."):
             paragraph = doc.add_paragraph()
             doc.add_heading('Key Insights', level=2)
             paragraph = doc.add_paragraph()
-            st.session_state.tmp_table.drop_duplicates(inplace=True)
+            st.session_state.tmp_table_llama.drop_duplicates(inplace=True)
             columns = list(st.session_state.tmp_table_llama.columns)
             table = doc.add_table(rows=1, cols=len(columns), style="Table Grid")
             table.autofit = True
