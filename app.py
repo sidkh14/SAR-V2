d0c5c52c70ca0b38988e29b0edc17a0373ad6305
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 11:32:44 +0000

    suspect

--- app.py
+++ app.py
@@ -1160,13 +1160,25 @@ if st.session_state.llm == "GPT-3.5":
 elif st.session_state.llm == "Llama-2-13b":
     if st.button("Summarize",disabled=st.session_state.disabled):
         summ_dict = st.session_state.tmp_table.set_index('Question')['Answer'].to_dict()
-        query = "Provide a detailed summary of the text provided"
-        prompt_1 = f'''You are a fraud analyst. Analyse the text provided to give a detailed summary by reframing the sentences to create a sequence of events.\n\n\
-            Question: {query}\n\
-            Context: {summ_dict}\n\                      
-            Response: (Provide the summary in a single paragraph with proper spacing between words.Keep font size same for each word )'''
-        summ = llama_llm(llama_13b,prompt_1)  
-        st.write(summ)
+        # query = "Provide a detailed summary of the text provided"
+        # prompt_1 = f'''You are a fraud analyst. Analyse the text provided to give a detailed summary by reframing the sentences to create a sequence of events.\n\n\
+        #     Question: {query}\n\
+        #     Context: {summ_dict}\n\                      
+        #     Response: (Provide the summary in a single paragraph with proper spacing between words.Keep font size same for each word )'''
+        # summ = llama_llm(llama_13b,prompt_1)  
+        # st.write(summ)
+
+        template =  f"""Provide a detailed summary of the context provided. From key-value pair in the text, use only value to summarize.
+        Return the summary in a single paragraph with same font size and proper spacing between words.\n\n\
+        Context: {summ_dict}\n\
+        Response: (Provide the summary in a single paragraph.)"""
+        prompt = PromptTemplate(template=template,input_variable=["summ_dict"])
+        llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
+        summary = llm_chain_llama.run(summ_dict)
+        st.write(summary)
+
+
+
 
 
 with st.spinner("Downloading...."):
