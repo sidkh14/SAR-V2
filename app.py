011e2d42a68c7db0b8c247bb0cdb4031825bdca2
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 12:07:56 +0000

    summ

--- app.py
+++ app.py
@@ -1169,7 +1169,7 @@ elif st.session_state.llm == "Llama-2-13b":
         # st.write(summ)
 
 
-        template = """Write a detailed summary of the text provided. 
+        template = """You are a fraud analyst. Write a detailed summary of the text provided. 
         Return your response in a single paragraph.
         ```{text}```
         Response: """
