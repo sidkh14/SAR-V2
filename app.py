64f05d43a7c971e1f9b8e8257c70d4d206073d7a
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 11:36:55 +0000

    suspect

--- app.py
+++ app.py
@@ -1173,7 +1173,7 @@ elif st.session_state.llm == "Llama-2-13b":
         Return the summary in a single paragraph with same font size and proper spacing between words.\n\n\
         Context: {summ_dict}\n\
         Response: (Provide the summary in a single paragraph.)"""
-        prompt = PromptTemplate(template=template,input_variables=["summ_dict"])
+        prompt = PromptTemplate(template=template)
         llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
         summary = llm_chain_llama.run(summ_dict)
         st.write(summary)
