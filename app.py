de6d2e8f05363bf741e8c41cbdb78f297ed5cfa5
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 10:52:01 +0000

    suspect

--- app.py
+++ app.py
@@ -773,7 +773,7 @@ with st.spinner('Wait for it...'):
             Hence, Compare the names present in the context. 
             Reply the name of the person who is basically the suspect.\n\n\
                             Context: {context_1}\n\
-                            Response: (Give me a concise response in one sentence.Do not use [provided document,context,based on the document] in the Response.Do not give me any Explanation,note)'''
+                            Response: (Give me a concise response in one sentence.Do not use [provided document,context,based on the document,provided context] in the Response.Do not give me any Explanation,note)'''
                 response = llama_llm(llama_13b,prompt_1)
                 chat_history[query] = response
 
