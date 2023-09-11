c49f5b8c0762ef3ed091c0407f2cd121ad58b365
corprt <143188080+corprt@users.noreply.github.com>
2023-09-11 12:51:29 +0000

    summ chnages

--- app.py
+++ app.py
@@ -1172,7 +1172,7 @@ elif st.session_state.llm == "Llama-2-13b":
 
 
         template = """Write a detailed summary of the text provided. 
-        Return your response professionally in a single paragraph.
+        Return your response in a single paragraph.
         ```{text}```
         Response: """
         prompt = PromptTemplate(template=template,input_variables=["text"])
