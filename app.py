385a1e7d77cbc8c49e1d1e2c5946a48c24ff14c8
corprt <143188080+corprt@users.noreply.github.com>
2023-09-12 07:32:20 +0000

    path

--- app.py
+++ app.py
@@ -1427,7 +1427,7 @@ if st.button("Submit"):
 # # Allow the user to clear all stored conversation sessions
 if st.button("Reset Session"):
     reset_session_state()
-    pdf_files_.clear()
+    # pdf_files_.clear()
     os.remove(temp_file_path)
     os.rmdir(tmp_dir_)
 
