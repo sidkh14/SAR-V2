07133a6cd3b2d6db01dc6a5e54711c8b06d452b3
corprt <143188080+corprt@users.noreply.github.com>
2023-09-12 07:53:37 +0000

    name

--- app.py
+++ app.py
@@ -771,8 +771,8 @@ with st.spinner('Wait for it...'):
 
                 query = "What is the victim's name?"
                 context_1 = docsearch.similarity_search(query, k=5)
-                prompt_1 = f'''You are a professional fraud analyst. Perform Name Enitity Recognition to identify the victim name as accurately as possible, given the context. The victim can also be referenced as the customer with whom the Fraud has taken place.
-                victim name can be identified from Cardholder Information.\n\n\
+                prompt_1 = f'''You are a professional fraud analyst. Perform Name Enitity Recognition to identify the victim's name as accurately as possible, given the context. The victim can also be referenced as the customer with whom the Fraud has taken place.
+                victim's name is the Name provided in Cardholder Information.\n\n\
                         Question: {query}\n\
                         Context: {context_1}\n\
                         Response: (Give me response in one sentence. Do not give me any Explanation or Note)'''
