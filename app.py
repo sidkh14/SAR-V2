b7ae7f08c9d0fdd7bfbc2628df5eaf98204d110e
corprt <143188080+corprt@users.noreply.github.com>
2023-09-12 10:23:05 +0000

    summ changes

--- app.py
+++ app.py
@@ -1196,243 +1196,243 @@ tmp_summary = []
 tmp_table = pd.DataFrame()
 
 if st.session_state.llm == "Open-AI":
-st.session_state.disabled=False
-tmp_table = pd.concat([tmp_table, st.session_state["tmp_table_gpt"]], ignore_index=True)
-tmp_summary.append(st.session_state["tmp_summary_gpt"])
+    st.session_state.disabled=False
+    tmp_table = pd.concat([tmp_table, st.session_state["tmp_table_gpt"]], ignore_index=True)
+    tmp_summary.append(st.session_state["tmp_summary_gpt"])
 
 
 elif st.session_state.llm == "Open-Source":
-st.session_state.disabled=False
-tmp_summary.append(st.session_state["tmp_summary_llama"])
-tmp_table = pd.concat([tmp_table, st.session_state["tmp_table_llama"]], ignore_index=True)
+    st.session_state.disabled=False
+    tmp_summary.append(st.session_state["tmp_summary_llama"])
+    tmp_table = pd.concat([tmp_table, st.session_state["tmp_table_llama"]], ignore_index=True)
 
 
 try:
-# initiate the doc file
-doc = docx.Document()
-# doc.add_section(WD_SECTION.NEW_PAGE)
-doc.add_heading(f"Case No.: {st.session_state.case_num}",0)
+    # initiate the doc file
+    doc = docx.Document()
+    # doc.add_section(WD_SECTION.NEW_PAGE)
+    doc.add_heading(f"Case No.: {st.session_state.case_num}",0)
 
-# Add a subheader for case details
-subheader_case = doc.add_paragraph("Case Details")
-subheader_case.style = "Heading 2"
-# Addition of case details
-paragraph = doc.add_paragraph(" ")
-case_info = {
-    "Case Number                            ": " SAR-2023-24680",
-    "Customer Name                       ": " John Brown",
-    "Customer ID                              ": " 9659754",
-    "Case open date                         ": " Feb 02, 2021",
-    "Case Type                                  ": " Fraud Transaction",
-    "Case Status                                ": " Open"
-}
-for key_c, value_c in case_info.items():
-    doc.add_paragraph(f"{key_c}: {value_c}")
-paragraph = doc.add_paragraph(" ")
+    # Add a subheader for case details
+    subheader_case = doc.add_paragraph("Case Details")
+    subheader_case.style = "Heading 2"
+    # Addition of case details
+    paragraph = doc.add_paragraph(" ")
+    case_info = {
+        "Case Number                            ": " SAR-2023-24680",
+        "Customer Name                       ": " John Brown",
+        "Customer ID                              ": " 9659754",
+        "Case open date                         ": " Feb 02, 2021",
+        "Case Type                                  ": " Fraud Transaction",
+        "Case Status                                ": " Open"
+    }
+    for key_c, value_c in case_info.items():
+        doc.add_paragraph(f"{key_c}: {value_c}")
+    paragraph = doc.add_paragraph(" ")
 
-# Add a subheader for customer info to the document ->>
-subheader_paragraph = doc.add_paragraph("Customer Information")
-subheader_paragraph.style = "Heading 2"
-paragraph = doc.add_paragraph(" ")
+    # Add a subheader for customer info to the document ->>
+    subheader_paragraph = doc.add_paragraph("Customer Information")
+    subheader_paragraph.style = "Heading 2"
+    paragraph = doc.add_paragraph(" ")
 
-# Add the customer information
-customer_info = {
-    "Name                                           ": " John Brown",
-    "Address                                      ": " 858 3rd Ave, Chula Vista, California, 91911 US",
-    "Phone                                          ": " (619) 425-2972",
-    "A/C No.                                        ": " 4587236908230087",
-    "SSN                                               ": " 653-30-9562"
-}
+    # Add the customer information
+    customer_info = {
+        "Name                                           ": " John Brown",
+        "Address                                      ": " 858 3rd Ave, Chula Vista, California, 91911 US",
+        "Phone                                          ": " (619) 425-2972",
+        "A/C No.                                        ": " 4587236908230087",
+        "SSN                                               ": " 653-30-9562"
+    }
 
-for key, value in customer_info.items():
-    doc.add_paragraph(f"{key}: {value}")
-paragraph = doc.add_paragraph()
-# Add a subheader for Suspect infor to the document ->>
-subheader_paragraph = doc.add_paragraph("Suspect's Info")
-subheader_paragraph.style = "Heading 2"
-paragraph = doc.add_paragraph()
-#""" Addition of a checkbox where unticked box imply unavailability of suspect info"""
+    for key, value in customer_info.items():
+        doc.add_paragraph(f"{key}: {value}")
+    paragraph = doc.add_paragraph()
+    # Add a subheader for Suspect infor to the document ->>
+    subheader_paragraph = doc.add_paragraph("Suspect's Info")
+    subheader_paragraph.style = "Heading 2"
+    paragraph = doc.add_paragraph()
+    #""" Addition of a checkbox where unticked box imply unavailability of suspect info"""
 
-# Add the customer information
-sent_val = "Suspect has been reported."
-paragraph = doc.add_paragraph()
-runner = paragraph.add_run(sent_val)
-runner.bold = True
-runner.italic = True
-suspect_info = {
-    "Name                                           ": "Mike White",
-    "Address                                      ": "520, WintergreenCt,Vancaville,CA,95587",
-    "Phone                                          ": "",
-    "SSN                                               ": "",
-    "Relationship with Customer ": ""
-}
+    # Add the customer information
+    sent_val = "Suspect has been reported."
+    paragraph = doc.add_paragraph()
+    runner = paragraph.add_run(sent_val)
+    runner.bold = True
+    runner.italic = True
+    suspect_info = {
+        "Name                                           ": "Mike White",
+        "Address                                      ": "520, WintergreenCt,Vancaville,CA,95587",
+        "Phone                                          ": "",
+        "SSN                                               ": "",
+        "Relationship with Customer ": ""
+    }
 
-for key, value in suspect_info.items():
-    doc.add_paragraph(f"{key}: {value}")
+    for key, value in suspect_info.items():
+        doc.add_paragraph(f"{key}: {value}")
 
-doc.add_heading('Summary', level=2)
-paragraph = doc.add_paragraph()
-doc.add_paragraph(tmp_summary)
-paragraph = doc.add_paragraph()
-doc.add_heading('Key Insights', level=2)
-paragraph = doc.add_paragraph()
-tmp_table.drop_duplicates(inplace=True)
-columns = list(tmp_table.columns)
-table = doc.add_table(rows=1, cols=len(columns), style="Table Grid")
-table.autofit = True
-for col in range(len(columns)):
-    # set_cell_margins(table.cell(0, col), top=100, start=100, bottom=100, end=50) # set cell margin
-    table.cell(0, col).text = columns[col]
-# doc.add_table(st.session_state.tmp_table.shape[0]+1, st.session_state.tmp_table.shape[1], style='Table Grid')
+    doc.add_heading('Summary', level=2)
+    paragraph = doc.add_paragraph()
+    doc.add_paragraph(tmp_summary)
+    paragraph = doc.add_paragraph()
+    doc.add_heading('Key Insights', level=2)
+    paragraph = doc.add_paragraph()
+    tmp_table.drop_duplicates(inplace=True)
+    columns = list(tmp_table.columns)
+    table = doc.add_table(rows=1, cols=len(columns), style="Table Grid")
+    table.autofit = True
+    for col in range(len(columns)):
+        # set_cell_margins(table.cell(0, col), top=100, start=100, bottom=100, end=50) # set cell margin
+        table.cell(0, col).text = columns[col]
+    # doc.add_table(st.session_state.tmp_table.shape[0]+1, st.session_state.tmp_table.shape[1], style='Table Grid')
 
-for i, row in enumerate(tmp_table.itertuples()):
-    table_row = table.add_row().cells # add new row to table
-    for col in range(len(columns)): # iterate over each column in row and add text
-        table_row[col].text = str(row[col+1]) # avoid index by adding col+1
-# save document
-# output_bytes = docx.Document.save(doc, 'output.docx')
-# st.download_button(label='Download Report', data=output_bytes, file_name='evidence.docx', mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    for i, row in enumerate(tmp_table.itertuples()):
+        table_row = table.add_row().cells # add new row to table
+        for col in range(len(columns)): # iterate over each column in row and add text
+            table_row[col].text = str(row[col+1]) # avoid index by adding col+1
+    # save document
+    # output_bytes = docx.Document.save(doc, 'output.docx')
+    # st.download_button(label='Download Report', data=output_bytes, file_name='evidence.docx', mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
-bio = io.BytesIO()
-doc.save(bio)
-        # col_d1, col_d2 = st.columns(2)
-col_s1, col_d1, col_d2 = st.tabs(["Summarize","Download Report", "Download Case Package"])
+    bio = io.BytesIO()
+    doc.save(bio)
+            # col_d1, col_d2 = st.columns(2)
+    col_s1, col_d1, col_d2 = st.tabs(["Summarize","Download Report", "Download Case Package"])
 
-with col_s1:
-    
-    if st.session_state.llm == "Open-AI":
-        st.session_state.disabled=False
-        with st.spinner('Summarization ...'):
-            if st.button("Summarize",disabled=st.session_state.disabled):
-                summ_dict_gpt = st.session_state.tmp_table_gpt.set_index('Question')['Answer'].to_dict()
-                # chat_history = resp_dict_obj['Summary']
-                memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=300)
-                memory.save_context({"input": "This is the entire summary"}, 
-                                {"output": f"{summ_dict_gpt}"})
-                conversation = ConversationChain(
-                llm=llm, 
-                memory = memory,
-                verbose=True)
-                st.session_state["tmp_summary_gpt"] = conversation.predict(input="Provide a detailed summary of the text provided by reframing the sentences. Provide the summary in a single paragraph. Please don't include words like these: 'chat summary', 'includes information' in my final summary.")
-                # showing the text in a textbox
-                # usr_review = st.text_area("", value=st.session_state["tmp_summary_gpt"])
-                # if st.button("Update Summary"):
-                #     st.session_state["fin_opt"] = usr_review
-                st.write(st.session_state["tmp_summary_gpt"])
-
-            elif st.session_state.llm == "Open-Source":
-                st.session_state.disabled=False
-                with st.spinner('Summarization ...'):
-                    if st.button("Summarize",disabled=st.session_state.disabled):
-
-                        template = """Write a detailed summary.
-                        Return your response in a single paragraph.
-                        ```{text}```
-                        Response: """
-                        prompt = PromptTemplate(template=template,input_variables=["text"])
-                        llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
-
-                        summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
-                        text = []
-                        for key,value in summ_dict_llama.items():
-                            text.append(value)
-                        st.session_state["tmp_summary_llama"] = llm_chain_llama.run(text)
-                        st.write(st.session_state["tmp_summary_llama"])
-            
-with col_d1:
-# Applying to download button -> download_button
-    st.markdown("""
-        <style>
-            .stButton download_button {
-                width: 100%;
-                height: 70%;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
-
-    # combined_doc_path = os.path.join(tmp_dir, "resulting_document.docx")
-    # doc.save(combined_doc_path)
-
-    # # Create a zip file with the uploaded PDF files and the combined document
-    # zip_file_name = "package_files.zip"
-    # if pdf_files:
-    #     st.write(file_paths)
-    #     files =  [combined_doc_path]
-    #     st.write(files)
+    with col_s1:
         
-    #     create_zip_file(files, zip_file_name)
-    #     # create_zip_file(file_paths, zip_file_name)
-    # else:
-    #     pass
-    # # Download the package
-    # with open(zip_file_name, "rb") as file:
-    #     st.download_button(
-    #         label="Download Case Package", 
-    #         data=file, 
-    #         file_name=zip_file_name,
-    #         disabled=st.session_state.disabled)
-    
-    if doc:
-        st.download_button(
-            label="Download Report",
-            data=bio.getvalue(),
-            file_name="Report.docx",
-            mime="docx",
-            disabled=st.session_state.disabled
-        )
-with col_d2:
+        if st.session_state.llm == "Open-AI":
+            st.session_state.disabled=False
+            with st.spinner('Summarization ...'):
+                if st.button("Summarize",disabled=st.session_state.disabled):
+                    summ_dict_gpt = st.session_state.tmp_table_gpt.set_index('Question')['Answer'].to_dict()
+                    # chat_history = resp_dict_obj['Summary']
+                    memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=300)
+                    memory.save_context({"input": "This is the entire summary"}, 
+                                    {"output": f"{summ_dict_gpt}"})
+                    conversation = ConversationChain(
+                    llm=llm, 
+                    memory = memory,
+                    verbose=True)
+                    st.session_state["tmp_summary_gpt"] = conversation.predict(input="Provide a detailed summary of the text provided by reframing the sentences. Provide the summary in a single paragraph. Please don't include words like these: 'chat summary', 'includes information' in my final summary.")
+                    # showing the text in a textbox
+                    # usr_review = st.text_area("", value=st.session_state["tmp_summary_gpt"])
+                    # if st.button("Update Summary"):
+                    #     st.session_state["fin_opt"] = usr_review
+                    st.write(st.session_state["tmp_summary_gpt"])
 
-    # initiating a temp file
-    tmp_dir = tempfile.mkdtemp()
+                elif st.session_state.llm == "Open-Source":
+                    st.session_state.disabled=False
+                    with st.spinner('Summarization ...'):
+                        if st.button("Summarize",disabled=st.session_state.disabled):
 
-    file_paths= []
+                            template = """Write a detailed summary.
+                            Return your response in a single paragraph.
+                            ```{text}```
+                            Response: """
+                            prompt = PromptTemplate(template=template,input_variables=["text"])
+                            llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
 
-    for uploaded_file in pdf_files:
-        file_pth = os.path.join(tmp_dir, uploaded_file.name)
-        with open(file_pth, "wb") as file_opn:
-            file_opn.write(uploaded_file.getbuffer())
-        file_paths.append(file_pth)
+                            summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
+                            text = []
+                            for key,value in summ_dict_llama.items():
+                                text.append(value)
+                            st.session_state["tmp_summary_llama"] = llm_chain_llama.run(text)
+                            st.write(st.session_state["tmp_summary_llama"])
+                
+    with col_d1:
+    # Applying to download button -> download_button
+        st.markdown("""
+            <style>
+                .stButton download_button {
+                    width: 100%;
+                    height: 70%;
+                }
+            </style>
+        """, unsafe_allow_html=True)
 
-    for fetched_pdf in fetched_files:
-        # st.write(fetched_pdf)
-        file_pth = os.path.join('data/', fetched_pdf)
-        # st.write(file_pth)
-        file_paths.append(file_pth)
 
-    
-    combined_doc_path = os.path.join(tmp_dir, "report.docx")
-    doc.save(combined_doc_path)
+        # combined_doc_path = os.path.join(tmp_dir, "resulting_document.docx")
+        # doc.save(combined_doc_path)
+
+        # # Create a zip file with the uploaded PDF files and the combined document
+        # zip_file_name = "package_files.zip"
+        # if pdf_files:
+        #     st.write(file_paths)
+        #     files =  [combined_doc_path]
+        #     st.write(files)
+            
+        #     create_zip_file(files, zip_file_name)
+        #     # create_zip_file(file_paths, zip_file_name)
+        # else:
+        #     pass
+        # # Download the package
+        # with open(zip_file_name, "rb") as file:
+        #     st.download_button(
+        #         label="Download Case Package", 
+        #         data=file, 
+        #         file_name=zip_file_name,
+        #         disabled=st.session_state.disabled)
+        
+        if doc:
+            st.download_button(
+                label="Download Report",
+                data=bio.getvalue(),
+                file_name="Report.docx",
+                mime="docx",
+                disabled=st.session_state.disabled
+            )
+    with col_d2:
+
+        # initiating a temp file
+        tmp_dir = tempfile.mkdtemp()
+
+        file_paths= []
+
+        for uploaded_file in pdf_files:
+            file_pth = os.path.join(tmp_dir, uploaded_file.name)
+            with open(file_pth, "wb") as file_opn:
+                file_opn.write(uploaded_file.getbuffer())
+            file_paths.append(file_pth)
+
+        for fetched_pdf in fetched_files:
+            # st.write(fetched_pdf)
+            file_pth = os.path.join('data/', fetched_pdf)
+            # st.write(file_pth)
+            file_paths.append(file_pth)
+
+        
+        combined_doc_path = os.path.join(tmp_dir, "report.docx")
+        doc.save(combined_doc_path)
 
 
 
-    # Create a zip file with the uploaded PDF files and the combined document
-    zip_file_name = "package_files.zip"
-    if file_paths:
-        files =  [combined_doc_path] + file_paths
-        create_zip_file(files, zip_file_name)
-        # create_zip_file(file_paths, zip_file_name)
-    else:
-        pass
+        # Create a zip file with the uploaded PDF files and the combined document
+        zip_file_name = "package_files.zip"
+        if file_paths:
+            files =  [combined_doc_path] + file_paths
+            create_zip_file(files, zip_file_name)
+            # create_zip_file(file_paths, zip_file_name)
+        else:
+            pass
 
-    
-    # Download the package
+        
+        # Download the package
 
-    with open(zip_file_name, "rb") as file:
-        st.download_button(
-            label="Download Case Package", 
-            data=file, 
-            file_name=zip_file_name,
-            disabled=st.session_state.disabled)
+        with open(zip_file_name, "rb") as file:
+            st.download_button(
+                label="Download Case Package", 
+                data=file, 
+                file_name=zip_file_name,
+                disabled=st.session_state.disabled)
 
-        # # Cleanup: Delete the temporary directory and its contents
-        # for file_path in file_paths + [combined_doc_path]:
-        #     os.remove(file_path)
-        # os.rmdir(temp_dir)
+            # # Cleanup: Delete the temporary directory and its contents
+            # for file_path in file_paths + [combined_doc_path]:
+            #     os.remove(file_path)
+            # os.rmdir(temp_dir)
 
-    
+        
 except NameError:
-pass
+    pass
 
 
 # Adding Radio button
