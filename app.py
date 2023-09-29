3c5e615f201ad10bb3133a86a1e721eb611f0d99
Siddhant Khandelwal <44205451+sidkh14@users.noreply.github.com>
2023-09-29 18:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1836,7 +1836,7 @@ elif selected_option_case_type == "AML":
                                 Only include transactions which are greater than or equal to 10000$ in your response. \n
                                 Give all such suspicious transactions grouped by transaction type(Credit card, savings account,etc.) \
                                 along with dates, amounts from the context as your response \
-                                Do not include any credit card payment transaction from savings account statement in your response. Also, Do not repeat the above information and provide a to the point response.\n\n
+                                Do not include any credit card payment transaction (Ex- Towards ABC Credit Card payment) from savings account statement in your response. Also, Do not repeat the above information and provide a to the point response.\n\n
                                 Context: {context_1}\n\
                                 Response: (Give me a concise response .Do not give me any Explanation,Note, etc.)'''
 
