cf3ea0d3385ea5bf1aea5aa38353f8b1c3be3cd5
Siddhant Khandelwal <44205451+sidkh14@users.noreply.github.com>
2023-09-29 18:40:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1826,14 +1826,17 @@ elif selected_option_case_type == "AML":
                     query = "What are the transaction that can be associated with Money Laundering activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
                     prompt_1 =  f'''You Are an Anti-Money Laundering Specialist, Identify the transactions \
-                                that can be potentially associated with the Money Laundering activity both from Credit Card transaction statement as well as savings account statement collectively. \n
+                                that can be potentially associated with the Money Laundering activity both from Credit Card \
+                                transaction statement as well as savings account statement collectively. \n
                                 Money laundering transactions often involve characteristics like large cash deposits greater than or equal to $10,000 \
-                                Payments greater than or equal to 10000$ to an unrecognized entity with no specific business purpose, \ 
-                                , transactions involving movement of funds to or from high-risk locations(Ex- Mauritious, Syria, Nigeria,etc.) and are greater than 10000$, any suspicion of money laundered via structuring , layering or intergration, process, \
-                                Cash deposits greater than or equal to 10000$ with source of funds not clear used to pay off credit card debt, etc\n \n
+                                Payments greater than or equal to 10000$ to an unrecognized entity with no specific business purpose,\
+                                transactions involving movement of funds to or from high-risk locations(Ex- Mauritious, Syria, Nigeria,etc.) \
+                                and are greater than 10000$, any suspicion of money laundered via structuring , layering or intergration, process, \
+                                Cash deposits greater than or equal to 10000$ with source of funds not clear used to pay off credit card debt, etc\n
                                 Only include transactions which are greater than or equal to 10,000$ in your response. \n
-                                Give all such suspicious transactions grouped by transaction type(Credit card, savings account,etc.) along with dates, amounts from the context as your response \
-                                Do not include any credit card payment transaction in your response. Do not repeat the above information and provide a to the point response.\n\n
+                                Give all such suspicious transactions grouped by transaction type(Credit card, savings account,etc.) \
+                                along with dates, amounts from the context as your response \
+                                Do not include any credit card payment transaction from savings account statement in your response. Also, Do not repeat the above information and provide a to the point response.\n\n
                                 Context: {context_1}\n\
                                 Response: (Give me a concise response .Do not give me any Explanation,Note, etc.)'''
 
