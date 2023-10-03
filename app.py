e200f6c5212f74c5b51d4008c61f8361c0985cb7
Siddhant Khandelwal <44205451+sidkh14@users.noreply.github.com>
2023-10-03 14:57:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1832,7 +1832,7 @@ elif selected_option_case_type == "AML":
                                 Payments greater than or equal to $10000 to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.)\
                                 transactions involving movement of funds to or from high-risk locations(Ex- Mauritious, Syria, Nigeria,etc.) \
                                 and are greater than $10000, any suspicion of money laundered via structuring , layering or intergration, process, \
-                                Cash deposits greater than or equal to $10000 with source of funds not clear used to pay off credit card debt, etc\n
+                                Cash deposits greater than or equal to 10000$ with source of funds not clear used to pay off credit card debt, etc\n
                                 Only include transactions which are greater than or equal to 10000$ from both the accounts in your response. \n
                                 Give all such suspicious transactions grouped by transaction type(Credit card, savings account,etc.) \
                                 along with dates, amounts from the context as your response \
