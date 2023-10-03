8657cadf7236cf9df46a51943cf40609487da54a
Siddhant Khandelwal <44205451+sidkh14@users.noreply.github.com>
2023-10-03 19:15:27 +0530

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -1806,7 +1806,7 @@ elif selected_option_case_type == "AML":
                   
                     query = "Is there any potential Money Laundering activity based on the transaction statements?"
                     context_1 = docsearch.similarity_search(query, k=5)
-                    prompt_1 = f'''You Are an Anti-Money Laundering Specialist who is an expert in detecting Money-laundering activities. \n
+                    prompt_1 = f'''You Are an Anti-Money Laundering Specialist who is an expert in detecting Money-laundering activity. \n
                     You sholud closely look into the credit card transaction statement as well as savings account transaction statement collectively and evaluate \
                     them together to check for any potential suspicious money laundering activities. \n
                     A Money laundering activity can be detected if any of the following transaction patterns is observed-:
@@ -1829,7 +1829,7 @@ elif selected_option_case_type == "AML":
                     query = "What are the transaction that can be associated with Money Laundering activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
                     prompt_1 =  f'''You Are an Anti-Money Laundering Specialist, Identify the transactions \
-                                that can be potentially associated with the Money Laundering activity both from Credit Card \
+                                that can be potentially associated with Money Laundering activity both from Credit Card \
                                 transaction statement as well as savings account statement collectively. \n
                                 Money laundering transactions often involve characteristics like large cash deposits greater than or equal to $10,000 \
                                 Payments greater than or equal to $10000 to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.)\
